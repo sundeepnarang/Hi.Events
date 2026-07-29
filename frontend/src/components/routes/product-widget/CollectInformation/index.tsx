ced96c1eb15b8dc0d16162cc1f9d578743f7c97e
Sundeep Narang <sundeepnrng@gmail.com>
2026-07-29 02:55:32 -0400

    feat: track Meta Pixel complete_registration event on order completion

--- frontend/src/components/routes/product-widget/CollectInformation/index.tsx
+++ frontend/src/components/routes/product-widget/CollectInformation/index.tsx
@@ -350,6 +350,7 @@ export const CollectInformation = () => {
     }
 
     if (order?.status === 'COMPLETED') {
+        trackMetaPixel("complete_registration", {id: order?.id});
         return <HomepageInfoMessage
             message={t`This order is complete`}
             link={eventCheckoutPath(eventId, orderShortId, 'summary')}
